144ae0bee97ff8da9b38c32b13162a8faa5e212d
jynnie <jynnie@mit.edu>
2018-12-14 22:01:03 -0500

    new buttons new feel

--- schemas/quests.js
+++ schemas/quests.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 
-const Quest = mongoose.model("Quest", { id: Number, clicks: Number });
+const Quest = mongoose.model("Quest", {
+  id: Number,
+  descript: String,
+  target: Number,
+  clicks: Number,
+  clickers: Number,
+  status: Boolean
+});
 
 module.exports = Quest;
